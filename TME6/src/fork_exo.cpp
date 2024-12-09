d44baa716c8fd0e13794fca0fe369cb2ab9eb5e9
houard <romain.houard@etu.u-paris.fr>
2024-12-10 00:13:48 +0100

    TM6

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <unistd.h>
-
+#include <vector>
+#include <sys/wait.h>
 int main () {
 	const int N = 3;
 	std::cout << "main pid=" << getpid() << std::endl;
+	std::vector<pid_t> child;
+	int nbChild = 0;
+	for (int i=1, j=N; i<=N && j==N ; i++ ) {
+		if(fork() != 0) break;
 
-	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
+		nbChild = 0;
+		child.clear();
+		std :: cout << getpid()<<std::endl;
+		child.push_back(getpid());
 		std::cout << " i:j " << i << ":" << j << std::endl;
 		for (int k=1; k<=i && j==N ; k++) {
-			if ( fork() == 0) {
+			if ( fork() == 0){
+				nbChild = 0;
+				child.clear();
+				std :: cout << getpid()<<std::endl;
+				child.push_back(getpid());
 				j=0;
 				std::cout << " k:j " << k << ":" << j << std::endl;
+				exit(0);
+			}
+			else{
+				nbChild++;
 			}
 		}
+
+
+	}
+
+	nbChild ++;
+
+	for (int i = 0 ; i < nbChild ; i++){
+		wait(nullptr);
 	}
 	return 0;
 }
---
+++ TME6/src/main.cpp
@@ -0,0 +1,85 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 23 oct. 2024
+ *      Author: 21412125
+ */
+#include <iostream>
+#include <unistd.h>
+#include <vector>
+#include <sys/wait.h>
+#include <ctime>
+#include "rsleep.h"
+int pv = 1;
+void sighandler(int signal){
+	--pv;
+	std::cout<< "pv restant : " <<pv <<" pid "<<getpid()<< std::endl;
+	if(pv == 0){
+		std::cout<< "T'es mort."<<getpid()<< std::endl;
+		exit(1);
+	}
+	return ;
+
+}
+void handlerDef(int signal){
+	std::cout<<"coup paré "<<getpid()<<std::endl;
+	return ;
+
+}
+void attaque(pid_t adversaire){
+	std::cout<<"Je suis " << getpid() <<" et je combat "<<adversaire<<std::endl;
+	struct sigaction act;
+	sigfillset(&act.sa_mask);
+	act.sa_flags = 0;
+	act.sa_handler = &sighandler;
+	sigaction(SIGINT,&act,nullptr);
+    pv--;
+	if(kill(adversaire,SIGINT)==-1){
+		std::cout<<"L'adversaire est mort."<<adversaire<<std::endl;
+		exit(0);
+	}
+
+	srand(time(nullptr));
+	int sleep_d=700000+rand()%300000;
+	usleep(sleep_d);
+
+}
+void defense (){
+	struct sigaction act;
+	sigfillset(&act.sa_mask);
+	act.sa_flags = 0;
+	//act.sa_handler = SIG_IGN;
+	act.sa_handler = &handlerDef;
+	sigaction(SIGINT,&act,nullptr);
+	sigset_t setPos;
+	sigemptyset(&setPos);
+	sigaddset(&setPos,SIGINT);
+	sigprocmask(SIG_BLOCK,&setPos,nullptr);
+	srand(time(nullptr));
+	int sleep_d=300000+rand()%700000;
+	usleep(sleep_d);
+	sigset_t setNeg;
+	sigfillset(&setNeg);
+	sigdelset(&setNeg,SIGINT);
+	sigsuspend(&setNeg);
+
+
+}
+
+void combat(pid_t adversaire){
+	while(true){
+		defense();
+		attaque(adversaire);
+	}
+}
+int main(){
+	pid_t pid = fork();
+	if(pid == 0){
+		combat(getppid());
+	}
+	else{
+		combat(pid);
+	}
+}
+
+
